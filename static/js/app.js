57ffbe3bc35a12ecbba5aff72b33ff786be3221d
ga35mav <ga35mav@mytum.de>
2017-04-24 18:08:55 +0200

    Implemented sorting when sort and order by options are selected.

--- static/js/app.js
+++ static/js/app.js
@@ -2,31 +2,89 @@ $(document).ready(function() {
     var BASE_URL = 'http://127.0.0.1:5000/api/v1';
     
     $('#system-search').keyup(function(event) {
-        if(!(/[a-zA-Z0-9]/.test(event.key))) {
+        var keyword = $('#system-search').val();
+        if(!keyword) {
+            //If keyword is cleared from the search box, than reset with the original default user's list
+            reset();
+        } else if(!validateSearchKey(keyword)) {
             return;
-        }else {
-            searchAndRender(event.target.value.trim());
         }
+        searchAndRender();
     });
     
-    var searchAndRender = function(keyword) {
-        //Get data from backend based on the search key word
-        $.get(BASE_URL+'/search/?query='+ keyword, function(data) {
-            if(data.results.length !== 0) {
-                $('.user-table tbody').empty();
-                data.results.forEach(user => {
-                    var row =   $('<tr><td>'+ user.id+'</td>' + 
-                                '<td>'+ user.first_name +'</td>' +
-                                '<td>' + user.last_name + '</td>' +
-                                '<td>' + user.gender + '</td>' +
-                                '<td>' + user.email + '</td>' +
-                                '<td>' + user.job_title + '</td></tr>');
-                   
-                    $('.user-table tbody').append(row);
-                });
-            };
-           
-        });
+    //Start searching when search button is clicked
+    $('.search-btn').click(function() {
+        var keyword = $('#system-search').val();
+        //If keyword is empty reset the view with default data
+        if(!keyword) {
+            reset();
+        }
+        
+        if(!validateSearchKey(keyword)) {
+            return;
+        }
+        searchAndRender();
+    });
+    
+    //Returns true if the string is alphanumeric string
+    var validateSearchKey = function(key) {
+        if((/^[a-z0-9]+$/i.test(key))) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    
+    var searchAndRender = function() {
+        var keyword = $('#system-search').val();
+        var sortByParam =  $('#sel1').val();
+        var orderByParam = $('#sel2').val();
+        
+        if(!keyword) {
+            //If search keyword is not present, use default sort endpoint
+            var endpoint = BASE_URL+'/sort/?sort_by='+ sortByParam +'&order='+ orderByParam;
+            $.get(endpoint, function(response) {
+                renderOnView(response.results);
+            });
+        } else {
+            //Get data from backend based on the search key word
+            var endpoint = BASE_URL+'/search/?query='+ keyword + '&sort_by=' + sortByParam +'&order='+ orderByParam;
+            $.get(endpoint, function(response) {
+                renderOnView(response.results);
+            });
+        }        
     }
 
+    //Accepts user data and populates on the table view
+    var renderOnView = function(data) {
+        if(data.length !== 0) {
+            $('.user-table tbody').empty();
+            data.forEach(user => {
+                var row =   $('<tr><td>'+ user.id+'</td>' + 
+                            '<td>'+ user.first_name +'</td>' +
+                            '<td>' + user.last_name + '</td>' +
+                            '<td>' + user.gender + '</td>' +
+                            '<td>' + user.email + '</td>' +
+                            '<td>' + user.job_title + '</td></tr>');
+
+                $('.user-table tbody').append(row);
+            });
+        };
+    }
+    
+    //Sort using the parameters used on the select options
+    $('#sel1').change(function(event) {
+        searchAndRender();
+    });
+    
+    $('#sel2').change(function(event) {
+        searchAndRender();
+    });
+    
+    //When search field is cleared reset table data to default user's list 
+    var reset = function() {
+        $('#sel1').val('id');
+        $('#sel2').val('asc');
+        searchAndRender();
+    }
 });
